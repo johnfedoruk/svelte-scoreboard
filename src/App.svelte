82fc63876ccd3284a17daabed46d474b42de45ca
John Fedoruk <johnny@johnfedoruk.ca>
2019-06-09 14:47:15 -0500

    Added show controls button to toggle control visibility

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,15 @@
 <script>
   let name = "John Doe";
   let points = 100;
+  let show_controls = false;
   const addPoint = () => {
     points += 1;
-  }
+  };
   const removePoint = () => {
     points -= 1;
+  };
+  const showControls = () => {
+    show_controls = !show_controls;
   }
 </script>
 
@@ -13,12 +17,24 @@
   h1 {
     color: purple;
   }
+  .show-controls {
+    color: black;
+    cursor: pointer;
+    width: 30px;
+    display: inline-block;
+    text-align: center;
+  }
 </style>
 
 <div>
-  <h1>{name}</h1>
-  <h3>{points||0}</h3>
-  <button on:click={addPoint}>+1</button>
-  <button on:click={removePoint}>-1</button>
-  <input type="number" bind:value={points}>
+  <h1>
+     {name}
+    <span on:click={showControls} class="show-controls">{#if show_controls}-{:else}+{/if}</span>
+  </h1>
+  <h3>{points || 0}</h3>
+  {#if show_controls}
+    <button on:click={addPoint}>+1</button>
+    <button on:click={removePoint}>-1</button>
+    <input type="number" bind:value={points} />
+  {/if}
 </div>
