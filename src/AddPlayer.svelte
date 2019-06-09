7de091202fe33866fe5c9cf83c286b9e42b56003
John Fedoruk <johnny@johnfedoruk.ca>
2019-06-09 15:01:46 -0500

    Added AddPlayer.svelte file with simple form

---
+++ src/AddPlayer.svelte
@@ -0,0 +1,12 @@
+<script>
+  let player = {
+    name: "",
+    points: 0,
+  }
+</script>
+
+<form>
+  <input type="text" placeholder="Name" bind:value={player.name}>
+  <input type="number" placeholder="Points" bind:value={player.points}>
+  <input type="submit" value="Add Player" >
+</form>
