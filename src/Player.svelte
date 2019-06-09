55eeeab049188c67939483859ae97d9df2f4520c
John Fedoruk <johnny@johnfedoruk.ca>
2019-06-09 14:51:37 -0500

    Refactored logic and view from App.svelte to Player.svelte

---
+++ src/Player.svelte
@@ -0,0 +1,40 @@
+<script>
+  let name = "John Doe";
+  let points = 100;
+  let show_controls = false;
+  const addPoint = () => {
+    points += 1;
+  };
+  const removePoint = () => {
+    points -= 1;
+  };
+  const showControls = () => {
+    show_controls = !show_controls;
+  }
+</script>
+
+<style>
+  h1 {
+    color: purple;
+  }
+  .show-controls {
+    color: black;
+    cursor: pointer;
+    width: 30px;
+    display: inline-block;
+    text-align: center;
+  }
+</style>
+
+<div>
+  <h1>
+     {name}
+    <span on:click={showControls} class="show-controls">{#if show_controls}-{:else}+{/if}</span>
+  </h1>
+  <h3>{points || 0}</h3>
+  {#if show_controls}
+    <button on:click={addPoint}>+1</button>
+    <button on:click={removePoint}>-1</button>
+    <input type="number" bind:value={points} />
+  {/if}
+</div>
