e82689915b6479c62399606e5582aadac6e4141c
John Fedoruk <johnny@johnfedoruk.ca>
2019-06-09 14:54:21 -0500

    Setup logic for rendering multiple players

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,10 @@
 <script>
   import Navbar from "./Navbar.svelte";
   import Player from "./Player.svelte";
+
+  let players = [
+
+  ];
 </script>
 
 <style>
@@ -8,4 +12,12 @@
 </style>
 
 <Navbar />
-<Player />
+<div>
+  {#if players.length === 0}
+    <p>No players</p>
+  {:else}
+    {#each players as player}
+      <Player />
+    {/each}
+  {/if}
+</div>
