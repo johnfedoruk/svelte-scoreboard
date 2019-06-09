01d139d03b45838ede122c8070534bdecdf99f27
John Fedoruk <johnny@johnfedoruk.ca>
2019-06-09 15:23:40 -0500

    AppPlayer.svelte dispatching new player

--- src/AddPlayer.svelte
+++ src/AddPlayer.svelte
@@ -1,12 +1,25 @@
 <script>
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
   let player = {
     name: "",
-    points: 0,
-  }
+    points: 0
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    dispatch("addPlayer", player);
+    player = {
+      name: "",
+      points: 0
+    };
+  };
 </script>
 
-<form>
-  <input type="text" placeholder="Name" bind:value={player.name}>
-  <input type="number" placeholder="Points" bind:value={player.points}>
-  <input type="submit" value="Add Player" >
+<form on:submit={onSubmit}>
+  <input type="text" placeholder="Name" bind:value={player.name} />
+  <input type="number" placeholder="Points" bind:value={player.points} />
+  <input type="submit" value="Add Player" />
 </form>
